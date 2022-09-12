3eb1c37cd902d2e4e654ca53f26c2684df552980
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2022-09-12 07:02:38 -0500

    Uso de función lambda para impresión de datos en main class

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
         //Creando arreglo de prueba para probar Permutation
         ArrayList<Integer> integerArrayList = new ArrayList<>();
-        integerArrayList.add(1);
         integerArrayList.add(3);
-        integerArrayList.add(2);
+        integerArrayList.add(1);
         integerArrayList.add(4);
-        integerArrayList.add(5);
 //        integerArrayList.add(6);
 //        integerArrayList.add(7);
 //        integerArrayList.add(8);
@@ -24,14 +22,14 @@ public class Main {
         personArrayList.add(new Person("Tasha", 23));
         //Se necesita un arreglo del tipo del argumento para poder almacenar la lista
         ArrayList<Stack<Integer>> permutaciones = Permutation.permute(integerArrayList);
-        System.out.println("CANTIDAD DE PERMUTACIONES: " + permutaciones.size());
-        //Ciclo para imprimir las permutaciones almacenadas en el arreglo permutaciones
-//        for (Stack<Integer> pila : permutaciones) {
-//            for (Integer dato : pila) {
-//                System.out.print(dato.toString() + " ");
-//            }
-//            System.out.println();
-//        }
+        //System.out.println("CANTIDAD DE PERMUTACIONES: " + permutaciones.size());
+        //Ciclo para imprimi r las permutaciones almacenadas en el arreglo permutaciones
+
+        permutaciones.forEach((permutacion) -> {
+            permutacion.forEach((dato) -> System.out.print(dato.toString() + " "));
+            System.out.println();
+        });
+
     }
 
 }
